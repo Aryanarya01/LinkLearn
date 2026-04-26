24f98c34ca282d840cbf0f5606f1bccf3c6681f7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 18:57:19 +0530

    register function

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,8 +1,12 @@
 
 
+export const register = async (req,res)=>{
+    
+}
+
 export const login = async(req,res)=>{
     try{
-
+        const 
     }catch(err){
         return res.status(500).json({message : "Server error!"});
     }
