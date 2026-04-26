233af1146b952551b26d8d64df98bb94a9ddf091
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 20:55:02 +0530

    register function

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,7 +1,16 @@
+import User from "../models/user.model";
 
 
 export const register = async (req,res)=>{
-    
+    try{
+        const {name, email, username, password} = req.body;
+        if(!name||!username||!email||!password){
+            return res.status(400).json("All fields are required");
+        }
+        const 
+    }catch(err){
+        return res.status(500).json({message : "Server Error!"})
+    }
 }
 
 export const login = async(req,res)=>{
