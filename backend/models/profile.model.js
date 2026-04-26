25cd1876d636fbeeabab538b82216fc4af6b7e78
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 21:10:05 +0530

    profile Schema

--- backend/models/profile.model.js
+++ backend/models/profile.model.js
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
 
 const workSchema = new mongoose.Schema({
-    
-})
+    company : {
+        type : String,
+        default : ""
+    },
+    position : {
+        type : String,
+        default : ""
+    },
+    years : {
+        type : String,
+        default : ""
+    }
+});
+
+
+
 const profileSchema = new mongoose.Schema({
     userId : {
         type : mongoose.Schema.Types.ObjectId,
@@ -13,6 +27,7 @@ const profileSchema = new mongoose.Schema({
         default : "",
     },
     pastWork : {
+        type : [workSchema],
         default : []
     },
     education : {
