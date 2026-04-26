f8c1067fb3698b85d8acb8b8cfa9ee4ed8d1d3c6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 18:47:00 +0530

    server setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors"
+import mongoose from "mongoose";
 const app = express();
 const port = 9090;
 dotenv.config();
 app.use(express.json());
 app.use(cors())
 
-
+app.get("/",(req,res)=>{
+    res.send("working")
+})
 const start = async()=>{
-    
+    const connectDb = await mongoose.connect("mongodb+srv://aryanarya0502:aryan555555@promeet.asxcnfo.mongodb.net/?appName=proMeet");
+    app.listen(port,()=>{
+        console.log(`App is listining to port :${port}`)
+    })
 }
+
+start();
