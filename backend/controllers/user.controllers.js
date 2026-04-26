afb1e64f0601d39bffd34cd8b79f5532b5fa3fc7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 21:20:41 +0530

    profile Schema

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -32,7 +32,20 @@ export const register = async (req,res)=>{
 
 export const login = async(req,res)=>{
     try{
-        const 
+        const {email, password} = req.body;
+        if(!email||!password){
+            return res.status(400).json({message : "All fields are required!"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message : "User not found!"});
+        }
+        const isMatched = await bcrypt.compare(password,user.password);
+        if(!isMatched){
+            return res.status(404).json({message : "Authentication Error!"});
+        }
+
+        const token = 
     }catch(err){
         return res.status(500).json({message : "Server error!"});
     }
