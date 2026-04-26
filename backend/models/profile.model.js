9064140f85f02447be371a6b2679b13e120b643c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 21:13:00 +0530

    profile Schema completed

--- backend/models/profile.model.js
+++ backend/models/profile.model.js
@@ -1,54 +1,54 @@
 import mongoose from "mongoose";
 
 const workSchema = new mongoose.Schema({
-    company : {
-        type : String,
-        default : ""
-    },
-    position : {
-        type : String,
-        default : ""
-    },
-    years : {
-        type : String,
-        default : ""
-    }
+  company: {
+    type: String,
+    default: "",
+  },
+  position: {
+    type: String,
+    default: "",
+  },
+  years: {
+    type: String,
+    default: "",
+  },
 });
 
 const educationSchema = new mongoose.Schema({
-    school : {
-        type : String,
-        default : "",
-    },
-    degree : {
-        type : String,
-        default : ""
-    },
-    fieldOfStudy : {
-        type : String,
-        default : ""
-    }
-})
+  school: {
+    type: String,
+    default: "",
+  },
+  degree: {
+    type: String,
+    default: "",
+  },
+  fieldOfStudy: {
+    type: String,
+    default: "",
+  },
+});
 
 const profileSchema = new mongoose.Schema({
-    userId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
-    },
-    bio : {
-        type : String,
-        default : "",
-    },
-    pastWork : {
-        type : [workSchema],
-        default : []
-    },
-    education : {
-        type : [educationSchema],
-        default:[]
-    }
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  pastWork: {
+    type: [workSchema],
+    default: [],
+  },
+  education: {
+    type: [educationSchema],
+    default: [],
+  },
+});
 
-const Profile = mongoose.model("Profile",profileSchema);
+const Profile = mongoose.model("Profile", profileSchema);
 
 export default Profile;
