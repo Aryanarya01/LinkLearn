90ef2cb5ae82b2053ffe0f00727aaded444cc651
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 21:08:07 +0530

    profile Schema

--- backend/models/profile.model.js
+++ backend/models/profile.model.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-
+const workSchema = new mongoose.Schema({
+    
+})
 const profileSchema = new mongoose.Schema({
     userId : {
         type : mongoose.Schema.Types.ObjectId,
