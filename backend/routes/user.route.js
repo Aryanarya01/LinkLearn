c92149fc328e24d8c1f5fc382508c51edb6f3841
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-27 00:06:54 +0530

    getAllUserProfils

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { getUserAndProfile, login, register, updateProfileData, updateUserProfile } from "../controllers/user.controllers.js";
+import { getAllUserProfile, getUserAndProfile, login, register, updateProfileData, updateUserProfile } from "../controllers/user.controllers.js";
 import { Protect } from "../middleware/protect.js";
 
 const router = Router();
@@ -10,4 +10,5 @@ router.route("/login").post(login)
 router.route("/user_update").post(Protect,updateUserProfile)
 router.route("/get_user_and_Profile").get(Protect,getUserAndProfile)
 router.route("/update_profile_data").post(Protect,updateProfileData)
+router.route("/user/get_all_users").get(Protect,getAllUserProfile)
 export default router;
