251d42dd4f0ebbd1fdbf925ce2e43999690905db
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 22:47:37 +0530

    protect middleware completed

--- backend/middleware/protect.js
+++ backend/middleware/protect.js
@@ -1,20 +1,20 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const Protect = async(req,res,next)=>{
-    try{
-            const token = req.cookies.token;
-            if(!token){
-                return res.status(404).json({message : "Not Authorized"});
-            }
-            const decoded = jwt.verify(token,process.env.JWT_SECRET)
-            const user = await User.findById(decoded.id).select("-password");
-            if(!user){
-                return res.status(404).json({message : "User not found!"});
-            }
-            req.user = user;
-            next();
-    }catch(err){
-        return res.status(500).json({message : "Server Error"})
+export const Protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(404).json({ message: "Not Authorized" });
     }
-}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
