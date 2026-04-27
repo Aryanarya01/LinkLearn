62c2756d7f853c9065a75d60f6457723409223d7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-27 21:05:56 +0530

    delte post

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -1,62 +1,62 @@
 import Posts from "../models/posts.model";
 import User from "../models/user.model";
 
-
-
-export const createPost = async(req,res)=>{
-    try{
-        const Id = req.user.id;
-        const user = await User.findById(Id);
-        if(!user){
-            return res.status(404).json({message : "User not found!"});
-        }
-
-        const post = new Posts({
-            userId : Id,
-            body : req.body.body,
-            media: req.file != undefined ? req.file.filename : "",
-            fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
-        })
-        await post.save();
-        return res.status(200).json({message : "Post Created"})
-    }catch(err){
-        return res.status(500).json({message : err.message})
+export const createPost = async (req, res) => {
+  try {
+    const Id = req.user.id;
+    const user = await User.findById(Id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
     }
-}
 
-export const getAllPosts = async(req,res)=>{
-    try{
-        const Id = req.user.id;
-        const user = await User.findById(Id);
-        if(!user){
-            return res.status(404).json({message : "User not found!"});
-        }
-        const posts = await Posts.find().populate("userId","name username email profilePicture");
-        await res.status(200).json({posts})
-    }catch(err){
-        return res.status(500).json({message : err.message})
+    const post = new Posts({
+      userId: Id,
+      body: req.body.body,
+      media: req.file != undefined ? req.file.filename : "",
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+    await post.save();
+    return res.status(200).json({ message: "Post Created" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const Id = req.user.id;
+    const user = await User.findById(Id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
     }
-}
+    const posts = await Posts.find().populate(
+      "userId",
+      "name username email profilePicture",
+    );
+    await res.status(200).json({ posts });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 
-
-export const deletePost = async(req,res)=>{
-    try{
-        const Id = req.user.id;
-        const {post_id} = req.body;
-        const user = await User.findById(Id);
-        if(!user){
-            return res.status(404).json({message : "User not found!"});
-        }
-        const postToDelete = await Posts.findOne({_id : post_id});
-        if(!postToDelete){
-            return res.status(404).json({message : "Post not found!"});
-        }
-        if(postToDelete.userId.toString()!== Id){
-            return res.status(400).json({message : "Not Authorized!"});
-        }
-        await postToDelete.deleteOne({_id : post_id});
-        return res.status(200).json({message : "Post Deleted!"})
-    }catch(err){
-        return res.status(500).json({message : err.message});
+export const deletePost = async (req, res) => {
+  try {
+    const Id = req.user.id;
+    const { post_id } = req.body;
+    const user = await User.findById(Id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
     }
-}
+    const postToDelete = await Posts.findOne({ _id: post_id });
+    if (!postToDelete) {
+      return res.status(404).json({ message: "Post not found!" });
+    }
+    if (postToDelete.userId.toString() !== Id) {
+      return res.status(400).json({ message: "Not Authorized!" });
+    }
+    await postToDelete.deleteOne({ _id: post_id });
+    return res.status(200).json({ message: "Post Deleted!" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
