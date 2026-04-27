94fb4f7d6b5ca6464730feadbffce13a5e8ecc11
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-27 20:56:42 +0530

    delte post

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -1,4 +1,4 @@
- import Posts from "../models/posts.model";
+import Posts from "../models/posts.model";
 import User from "../models/user.model";
 
 
@@ -21,5 +21,26 @@ export const createPost = async(req,res)=>{
         return res.status(200).json({message : "Post Created"})
     }catch(err){
         return res.status(500).json({message : err.message})
+    }
+}
+
+export const getAllPosts = async(req,res)=>{
+    try{
+        const Id = req.user.id;
+        const user = await User.findById(Id);
+        if(!user){
+            return res.status(404).json({message : "User not found!"});
+        }
+        const posts = await Posts.find().populate("userId","name username email profilePicture");
+        await res.status(200).json({posts})
+    }catch(err){
+        return res.status(500).json({message : err.message})
+    }
+}
+
+
+export const deletePost = async(req,res)=>{
+    try{
+        
     }
 }
