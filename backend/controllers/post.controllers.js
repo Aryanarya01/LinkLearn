d8aee85a40d6470ddef28fce8dd25584b528d0e3
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 00:23:14 +0530

    incLike completed

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -118,18 +118,17 @@ export const delete_comment_of_user = async (req, res) => {
   }
 };
 
-
-export const incrLikes = async(req,res)=>{
-  try{
-    const {post_id} = req.body;
+export const incrLikes = async (req, res) => {
+  try {
+    const { post_id } = req.body;
     const post = await Posts.findById(post_id);
-    if(!post){
-      return res.status(404).json({message : "Post not found"});
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
     post.likes = post.likes + 1;
     await post.save();
-    return res.status(200).json({message : "Like Incremented!"})
-  }catch(err){
-    return res.status(500).json({message : err.message})
+    return res.status(200).json({ message: "Like Incremented!" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
-}
+};
