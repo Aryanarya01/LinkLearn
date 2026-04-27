95a7289a420c6f4f6aafbd8bada3cc91352f62bb
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-27 21:22:33 +0530

    commentsmodel completed

--- backend/models/comments.model.js
+++ backend/models/comments.model.js
@@ -1,20 +1,19 @@
 import mongoose from "mongoose";
 
-
 const commentSchema = new mongoose.Schema({
-    userId : {
-        type:mongoose.Schema.Types.ObjectId,
-        ref : "User",
-    },
-    postId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Posts"
-    },
-    body : {
-        type : String,
-        required : true,
-    }
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Posts",
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+});
 
-const Comment = mongoose.model("Comment",commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 export default Comment;
