eec7f86605c4a06d692bca2166a76513c5b40702
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-27 21:20:14 +0530

    commentsmodel

--- backend/models/comments.model.js
+++ backend/models/comments.model.js
@@ -1,2 +1,13 @@
+import mongoose from "mongoose";
 
 
+const comentSchema = new mongoose.Schema({
+    userId : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "User",
+    },
+    body : {
+        type : String,
+        required : true,
+    }
+})
