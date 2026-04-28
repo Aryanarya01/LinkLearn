01b199c0cc3c877081751bfa59c293ddb08cb7ae
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 21:38:04 +0530

    login register completed

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,78 +1,101 @@
 "use client";
 
-import React, { useState } from 'react'
-import { clientServer } from '../config/page';
+import React, { useState } from "react";
+import { clientServer } from "../config/page";
 
 import { useRouter } from "next/navigation";
 const Login = () => {
-  const router = useRouter()
-  const [name,setName] = useState<string>("");
-  const [username,setUsername] = useState<string>("");
-  const [email,setEmail] = useState<string>("");
-  const [password,setPassword] = useState<string>("");
-  const [userLoginMethod,setUserLoginMethod] = useState<boolean>(false)
-  const handelRegister = async()=>{
-     
-      try{
-      const response =  await clientServer.post("/register",{
-          name,email,username,password
-        });
-        alert("User Registerd Successfully");
-        setUserLoginMethod(true)
-        router.push("/login")
-
-      }catch(err : any){
-          alert(err.message)
-      }
-  }
-
-  const handelLogin = async ()=>{
-   
-    try{  
-      const response = await clientServer.post("/login",{
-        email,password
-      })
-      alert("Login Successful");
-      router.push("/dashboard")
-    }catch(err : any){
+  const router = useRouter();
+  const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [userLoginMethod, setUserLoginMethod] = useState<boolean>(false);
+  const handelRegister = async () => {
+    try {
+      const response = await clientServer.post("/register", {
+        name,
+        email,
+        username,
+        password,
+      });
+      alert("User Registerd Successfully");
+      setUserLoginMethod(true);
+      router.push("/login");
+    } catch (err: any) {
       alert(err.message);
     }
-  }
+  };
 
+  const handelLogin = async () => {
+    try {
+      const response = await clientServer.post("/login", {
+        email,
+        password,
+      });
+      alert("Login Successful");
+      router.push("/dashboard");
+    } catch (err: any) {
+      alert(err.message);
+    }
+  };
 
   return (
     <div>
       <div>
         <div>
-        
-            {!userLoginMethod && ( 
+          {!userLoginMethod && (
             <div>
-              <input type="text" placeholder='Enter Name' value={name}  onChange={(e)=>{
-                setName(e.target.value)
-              }}/>
-              <input type="text" placeholder='Enter UserName' value={username}  onChange={(e)=>{
-                setUsername(e.target.value)
-              }}/>
+              <input
+                type="text"
+                placeholder="Enter Name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <input
+                type="text"
+                placeholder="Enter UserName"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
             </div>
-            )}
-            <input type="email" placeholder='Enter Email' value={email} onChange={(e)=>{
-              setEmail(e.target.value)
-            }}/>
-            <input type="password" placeholder='Enter Password' value={password} onChange={(e)=>{
-              setPassword(e.target.value)
-            }}/>
-            <button type='submit' onClick={()=>{
-              if(userLoginMethod){
+          )}
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            onClick={() => {
+              if (userLoginMethod) {
                 handelLogin();
-              }else{
+              } else {
                 handelRegister();
               }
-            }}>Sign Up</button>
-       
+            }}
+          >
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
