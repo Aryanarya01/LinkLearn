614c905ac9c3205200050890e56a35b60bef452b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 07:33:57 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 import express from "express";
-import dotenv from "dotenv"
-import cors from "cors"
+import dotenv from "dotenv";
+import cors from "cors";
 import mongoose from "mongoose";
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const port = 9090;
- 
+
 app.use(express.json());
 app.use(express.static("uploads"));
-app.use(cors())
-app.use(cookieParser())
-app.use(userRouter)
- 
-const start = async()=>{
-    const connectDb = await mongoose.connect("mongodb+srv://aryanarya0502:aryan555555@promeet.asxcnfo.mongodb.net/?appName=proMeet");
-    app.listen(port,()=>{
-        console.log(`App is listining to port :${port}`)
-    })
-}
+app.use(cors());
+app.use(cookieParser());
+app.use(userRouter);
+
+const start = async () => {
+  const connectDb = await mongoose.connect(
+    "mongodb+srv://aryanarya0502:aryan555555@promeet.asxcnfo.mongodb.net/?appName=proMeet",
+  );
+  app.listen(port, () => {
+    console.log(`App is listining to port :${port}`);
+  });
+};
 
 start();
