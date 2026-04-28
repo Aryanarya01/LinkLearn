166dbe0ab3ce87cf09345aca9bfd007dead5bf41
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 23:21:29 +0530

    context

---
+++ frontend/app/context/page.tsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+
+const UserContext = createContext<any>(null);
+
+
+export const UserProvider = ({children} :any)=>{
+    
+    const [user,setUser] = useState<any>(null)
+    return(
+        <UserContext.Provider value={{user,setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export const useUser = ()=>{
+    const context = useContext(UserContext);
+    if(!context){
+        throw new Error("useUser must be inside UserPrvider")
+    }
+    return context
+}
