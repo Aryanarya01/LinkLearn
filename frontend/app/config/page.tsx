1cf4c968e774575113a6aabd467ab453d1f34e60
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 17:00:23 +0530

    config

---
+++ frontend/app/config/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+import axios from "axios"
+
+export const BASE_URL = "localhost:9090";
+export const  clientServer = axios.create({
+    baseURL  : BASE_URL,
+    headers : {
+        "Content-Type" : "application/json",
+    },
+})
