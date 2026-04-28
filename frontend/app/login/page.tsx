9e7bedbbbf2263ff2f34e4ad06f2e0a7c73482e4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 20:47:19 +0530

    login register

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Login = () => {
+
+  const 
   return (
-    <div>Login</div>
+    <div>
+      <div>
+        <div>
+          <input type="text" placeholder='' value={}/>
+          <input type="text" placeholder='' value={} />
+          <input type="email" placeholder='' value={}/>
+          <input type="password" placeholder='' value={}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
