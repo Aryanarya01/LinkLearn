4e05c3bd29ab816a2d93480efb5bcd29f7d74b8c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 22:41:15 +0530

    Dashboard layout

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,21 +1,44 @@
-import React, { useEffect } from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from 'react'
 import UserLayout from '../userLayout/page'
 import DashboardLayout from '../dashboardLayout/page'
-import { BASE_URL } from '../config/page'
+import { BASE_URL, clientServer } from '../config/page'
+ 
 
 const Dashboard = () => {
-    
+    const route = useRouter()
    useEffect(()=>{
-    if(!localStorage.getItem("token")){
-        route
+    const token = localStorage.getItem("token")
+    if(token){
+        route.push("/login")
+    }else{
+        getAboutUser();
     }
-   })
+   },[])
+const [user,setUser] = useState<any>(null);
+
+
+const getAboutUser = async()=>{
+        try{
+            const response = await clientServer.get("/get_user_and_Profile");
+            setUser(response.data)
+            console.log(response);
+            
+        }catch(err : any){
+            alert(err.message)
+        }
+}
+
+
   return (
     <UserLayout>
         <DashboardLayout>
             <div>
                 <div className="searchBox">
-                    <img src={`${BASE_URL}/${}`}/>
+                    {user && user.userId && (
+  <img src={`${BASE_URL}/${user.userId.profilePicture}`} />
+)}
                 </div>
             </div>
         </DashboardLayout>
