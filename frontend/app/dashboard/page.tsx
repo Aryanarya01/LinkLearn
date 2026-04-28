d335aeae3bee25be8b3fb2a8c0bb5e962085c95c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 22:23:38 +0530

    Dashboard layout

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import UserLayout from '../userLayout/page'
+import DashboardLayout from '../dashboardLayout/page'
+import { BASE_URL } from '../config/page'
 
 const Dashboard = () => {
-
-   
+    
+   useEffect(()=>{
+    if(!localStorage.getItem("token")){
+        route
+    }
+   })
   return (
-    <div>Dashboard</div>
+    <UserLayout>
+        <DashboardLayout>
+            <div>
+                <div className="searchBox">
+                    <img src={`${BASE_URL}/${}`}/>
+                </div>
+            </div>
+        </DashboardLayout>
+    </UserLayout>
   )
 }
 
--- frontend/app/dashboardLayout/page.tsx
+++ frontend/app/dashboardLayout/page.tsx
@@ -2,13 +2,19 @@
 
 import React from 'react'
 
-const DashboardLayout = () => {
+const DashboardLayout = ({children}:any) => {
   return (
     <div>
         <div className="container">
-            <div className="left_container"></div>
-            <div className="mainContainer"></div>
-            <div className="right_container"></div>
+            <div className="left_container">
+                <h2>Scrool</h2>
+                <h2>Discover</h2>
+                <h2>My Connection</h2>
+            </div>
+            <div className="mainContainer">{children}</div>
+            <div className="right_container">
+                <h2>Top connection</h2>
+            </div>
         </div>
     </div>
   )
