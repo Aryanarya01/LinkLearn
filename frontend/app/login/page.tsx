254a6e774c6f52e6f1aa92ec6b409731a17b00b8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-28 20:55:58 +0530

    login register

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
 
-  const 
+  const [name,setName] = useState<string>("");
+  const [username,setUsername] = useState<string>("");
+  const [email,setEmail] = useState<string>("");
+  const [password,setPassword] = useState<string>("");
   return (
     <div>
       <div>
         <div>
-          <input type="text" placeholder='' value={}/>
-          <input type="text" placeholder='' value={} />
-          <input type="email" placeholder='' value={}/>
-          <input type="password" placeholder='' value={}/>
+          <input type="text" placeholder='' value={name}  onChange={(e)=>{
+            setName(e.target.value)
+          }}/>
+          <input type="text" placeholder='' value={username}  onChange={(e)=>{
+            setUsername(e.target.value)
+          }}/>
+          <input type="email" placeholder='' value={email} onChange={(e)=>{
+            setEmail(e.target.value)
+          }}/>
+          <input type="password" placeholder='' value={password} onChange={(e)=>{
+            setPassword(e.target.value)
+          }}/>
         </div>
       </div>
     </div>
