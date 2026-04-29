23ebb63a3b7562c8dd2ca82b00ddd871cf8bdebb
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-29 23:12:31 +0530

    profile

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import React from 'react'
+import { clientServer } from '../config/page'
 
 const Profile = () => {
 
 const getProfile = async()=>{
   try{
-
+      const response = await clientServer.get("/get_user_and_Profile")
   }catch(err : any){
     alert(err.message)
   }
