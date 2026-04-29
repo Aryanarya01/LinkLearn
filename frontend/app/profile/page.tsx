a9eded4be6c90f6edc4754b69e2d8ade2b382959
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-29 23:13:40 +0530

    profile

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { clientServer } from '../config/page'
 
 const Profile = () => {
-
+const [profile, setProfile] = useState("")
 const profileFetched = async()=>{
   try{
       const response = await clientServer.get("/get_user_and_Profile");
