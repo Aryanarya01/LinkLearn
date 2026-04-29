78a854dc9c314e3fd443f4997cf54145d0ee57b0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-29 21:45:46 +0530

    discover

--- frontend/app/discover/page.tsx
+++ frontend/app/discover/page.tsx
@@ -1,19 +1,29 @@
-
-
-import React from 'react'
-import UserLayout from '../userLayout/page'
-import DashboardLayout from '../dashboardLayout/page'
-
+import React, { useState } from "react";
+import UserLayout from "../userLayout/page";
+import DashboardLayout from "../dashboardLayout/page";
+import { clientServer } from "../config/page";
+const [profiles, setProfiles] = useState([]);
 const Discover = () => {
+  const getAllProfile = async () => {
+    try {
+      const response = await clientServer.get("/user/get_all_users");
+      const users = response.data.AllProfile;
+      setProfiles(users);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <UserLayout>
-        <DashboardLayout>
-            <div>
-                <h1>Discover</h1>
-            </div>
-        </DashboardLayout>
+      <DashboardLayout>
+        <div>
+          <h1>Discover</h1>
+          {}
+        </div>
+      </DashboardLayout>
     </UserLayout>
-  )
-}
+  );
+};
 
-export default Discover
+export default Discover;
