606aa6e6af38441164115bc5f233d6fa74a21434
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-29 23:02:44 +0530

    navbar

--- frontend/components/NavbarComponent/page.tsx
+++ frontend/components/NavbarComponent/page.tsx
@@ -1,13 +1,20 @@
+"use client"
+
 import React from 'react'
 import styles from "./page.module.css"
+import { useRouter } from 'next/navigation'
 const NavbarComponent = () => {
+  const router = useRouter();
+
   return (
     <div> 
         <nav className={styles.Navbar}>
           <h2>LinkLearn</h2>
 
           <div className={styles.authThing}>
-            <h3>Profile</h3>
+            <h3 onClick={()=>{
+              router.push("/profile")
+            }}>Profile</h3>
             <h3>Logout</h3>
           </div>
         </nav>
