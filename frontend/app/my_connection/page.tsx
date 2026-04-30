07106489c701d31cb5bda1b5f5285345a4de44e0
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-30 23:16:05 +0530

    myconnections

--- frontend/app/my_connection/page.tsx
+++ frontend/app/my_connection/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import UserLayout from '../userLayout/page'
 import DashboardLayout from '../dashboardLayout/page'
 
 const page = () => {
+
+    const [myConnections,setMyConnections] = useState();
+
+
+    const whatAreMyConnections = async()=>{
+        
+    }
   return (
      <UserLayout>
         <DashboardLayout>
@@ -10,6 +18,8 @@ const page = () => {
                 <h2>My Connections</h2>
                 <div>
                     <h4>My Network</h4>
+
+
                 </div>
                 
             </div>
