f0f12c3da457b45a6692928217046b25a799f702
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-30 23:23:58 +0530

    myconnections

--- frontend/app/my_connection/page.tsx
+++ frontend/app/my_connection/page.tsx
@@ -1,16 +1,26 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UserLayout from '../userLayout/page'
 import DashboardLayout from '../dashboardLayout/page'
+import { BASE_URL, clientServer } from '../config/page'
 
 const page = () => {
 
-    const [myConnections,setMyConnections] = useState();
+    const [myConnections,setMyConnections] = useState([]);
 
 
     const whatAreMyConnections = async()=>{
-        
+        try{
+            const response = await clientServer.get("/user/what_are_my_connection");
+            setMyConnections(response.data.myConnection)
+        }catch(err : any){
+            alert(err.message)
+        }
     }
+
+    useEffect(()=>{
+        whatAreMyConnections()
+    },[])
   return (
      <UserLayout>
         <DashboardLayout>
@@ -19,6 +29,12 @@ const page = () => {
                 <div>
                     <h4>My Network</h4>
 
+                {myConnections ? 
+                    <div>
+                        <img src={`${BASE_URL}/${myConnections.userId?.profilePicture}`} />
+                        <h3>{myConnections.userId?.name}</h3>
+                    </div>
+                : <h2>No Connection</h2>}
 
                 </div>
                 
