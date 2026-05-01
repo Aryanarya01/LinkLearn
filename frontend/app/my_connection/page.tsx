c1a0ca5ddf4c40b45c38e1f34d0d83e465f19bc6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-05-01 22:08:01 +0530

    accept connnection

--- frontend/app/my_connection/page.tsx
+++ frontend/app/my_connection/page.tsx
@@ -1,70 +1,73 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import UserLayout from '../userLayout/page'
-import DashboardLayout from '../dashboardLayout/page'
-import { BASE_URL, clientServer } from '../config/page'
+"use client";
+import React, { useEffect, useState } from "react";
+import UserLayout from "../userLayout/page";
+import DashboardLayout from "../dashboardLayout/page";
+import { BASE_URL, clientServer } from "../config/page";
 
 const page = () => {
+  const [myConnections, setMyConnections] = useState([]);
 
-    const [myConnections,setMyConnections] = useState([]);
-
-
-    const whatAreMyConnections = async()=>{
-        try{
-            const response = await clientServer.get("/user/what_are_my_connection");
-            setMyConnections(response.data.myConnection)
-        }catch(err : any){
-            alert(err.message)
-        }
+  const whatAreMyConnections = async () => {
+    try {
+      const response = await clientServer.get("/user/what_are_my_connection");
+      setMyConnections(response.data.myConnection);
+    } catch (err: any) {
+      alert(err.message);
     }
+  };
 
-    const accept_connection = async({requestId,action_type}:any)=>{
-        try{
-            const response = await clientServer.post("/user/accept_connection",{
-                requestId,
-                action_type
-            })
-             whatAreMyConnections(); 
-        }catch(err:any){
-            alert(err.message)
-        }
+  const accept_connection = async ({ requestId, action_type }: any) => {
+    try {
+      const response = await clientServer.post("/user/accept_connection", {
+        requestId,
+        action_type,
+      });
+      whatAreMyConnections();
+    } catch (err: any) {
+      alert(err.message);
     }
+  };
 
-    useEffect(()=>{
-        whatAreMyConnections()
-    },[])
+  useEffect(() => {
+    whatAreMyConnections();
+  }, []);
   return (
-     <UserLayout>
-        <DashboardLayout>
-            <div>
-                <h2>My Connections</h2>
-                <div>
-                    <h4>My Network</h4>
+    <UserLayout>
+      <DashboardLayout>
+        <div>
+          <h2>My Connections</h2>
+          <div>
+            <h4>My Network</h4>
 
-                {myConnections.length > 0 ? (
-                    myConnections.map((connec)=>(
-                        <div key={connec._id}>
-                        <img style={{width : "7rem",borderRadius : "50%"}} src={`${BASE_URL}/${connec.userId?.profilePicture}`} />
-                        <h3>{connec.userId?.name}</h3>
-                        <p>{connec.userId.username}</p>
-                        <button onClick={()=>{
-                            accept_connection({
-                                    requestId: connec._id,
-        action_type: "accept"
-                            })
-                        }}>accept</button>
-                        </div>
-                    )
-
-                     
-               ) ): <h2>No Connection</h2>}
-
+            {myConnections.length > 0 ? (
+              myConnections.map((connec) => (
+                <div key={connec._id}>
+                  <img
+                    style={{ width: "7rem", borderRadius: "50%" }}
+                    src={`${BASE_URL}/${connec.userId?.profilePicture}`}
+                  />
+                  <h3>{connec.userId?.name}</h3>
+                  <p>{connec.userId.username}</p>
+                  <button
+                    onClick={() => {
+                      accept_connection({
+                        requestId: connec._id,
+                        action_type: "accept",
+                      });
+                    }}
+                  >
+                    accept
+                  </button>
                 </div>
-                
-            </div>
-        </DashboardLayout>
-     </UserLayout>
-  )
-}
+              ))
+            ) : (
+              <h2>No Connection</h2>
+            )}
+          </div>
+        </div>
+      </DashboardLayout>
+    </UserLayout>
+  );
+};
 
-export default page
+export default page;
